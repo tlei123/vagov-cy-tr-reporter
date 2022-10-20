9bfd577f97d77cbcd5cbe43b7255cb6066ca9dbf
Tze-chiu Lei <tclei2009@gmail.com>
2022-10-19 17:21:44 -0700

    Replace chalk with colors.

--- src/lib/testrail-logger.ts
+++ src/lib/testrail-logger.ts
@@ -1,43 +1,43 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import chalk from 'chalk';
+import colors from 'colors';
 import util from 'util';
 
 export class TestRailLogger {
   log(text: string): void {
-    console.log(chalk.bold.yellow('[VCTR]', text));
+    console.log(colors.bold.yellow(`[VCTR] ${text}`));
   }
 
   logObj(text: string, obj: any): void {
     console.log(
-      chalk.bold.yellow('[VCTR]', text),
-      chalk.bold.yellow(util.inspect(obj, { colors: true, depth: null })),
+      colors.bold.yellow(`[VCTR] ${text}`),
+      colors.bold.yellow(util.inspect(obj, { colors: true, depth: null })),
     );
   }
 
   success(text: string): void {
-    console.log(chalk.bold.green('[VCTR]', text));
+    console.log(colors.bold.green(`[VCTR] ${text}`));
   }
 
   successObj(text: string, obj: any): void {
     console.log(
-      chalk.bold.green('[VCTR]', text),
-      chalk.bold.green(util.inspect(obj, { colors: true, depth: null })),
+      colors.bold.green(`[VCTR] ${text}`),
+      colors.bold.green(util.inspect(obj, { colors: true, depth: null })),
     );
   }
 
   error(text: string): void {
-    console.log(chalk.bold.red('[VCTR] ERROR:', text));
+    console.log(colors.bold.red(`[VCTR] ERROR: ${text}`));
   }
 
   errorObj(text: string, obj: any): void {
     console.log(
-      chalk.bold.red('[VCTR]', text),
-      chalk.bold.red(util.inspect(obj, { colors: true, depth: null })),
+      colors.bold.red(`[VCTR] ERROR: ${text}`),
+      colors.bold.red(util.inspect(obj, { colors: true, depth: null })),
     );
   }
 
   warn(text: string): void {
-    console.log(chalk.bold.keyword('orange')('[VCTR] WARNING:', text));
+    console.log(colors.bold.black.bgYellow(`[VCTR] WARNING: ${text}`));
   }
 }
 /* eslint-enable @typescript-eslint/no-explicit-any */
